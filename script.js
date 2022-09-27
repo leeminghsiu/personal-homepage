a493c5ffd7aa76b4b12f036a8277956ae55f5e8d
Dylan Tse <dylan@Dylans-MacBook-Air.local>
2022-09-27 08:48:39 -0700

    chore: code review comments and fixing typos

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Code Review Comment: 
+// Would love to see the JS script file organized in a separate folder
+
 function hideDonate() {
   document.querySelector(".donate").style.display = "none";
   document.querySelector(".choose").style.display = "flex";
